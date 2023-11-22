ee807406897019e1beb4e59230446a12758cf089
Dário Santos <dariosantos@tetrapi.pt>
2023-11-22 21:32:27 -0100

    Changed brief of main

--- main_team_3.c
+++ main_team_3.c
@@ -3,16 +3,15 @@
 
 #include "functions_team_3.h"
 
+/**
+ * @brief In this project, the program asks the user for 20 integers, of values ranging from 7 to 30, 
+ * which are then stored in an array. The provided integers are then used for either calculating statistics 
+ * or perform operations, at the user's request.
+ * 
+ * @return int 
+ */
 int main()
 {
-    /* ==========================================================================
-     * In this project, the program asks the user for 20 integers,
-     * of values ranging from 7 to 30, which are then stored in an array.
-     * The provided integers are then used for either calculating statistics
-     * or perform operations, at the user's request.
-     * ==========================================================================
-     */
-
     //* Variables ===============================================================
     int page = 1;
     char option;
